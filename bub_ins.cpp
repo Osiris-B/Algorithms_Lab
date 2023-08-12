4295b4387f99333a19199158df3736d7e6404491
Osiris-B <141070500+Osiris-B@users.noreply.github.com>
2023-08-12 12:43:11 +0530

    Update bub_ins.cpp

--- bub_ins.cpp
+++ bub_ins.cpp
@@ -45,39 +45,49 @@ void InsertionSort(vector<int> &arr)
   {
     cout << arr[i] << "\n";
   }
-  cout << "\nTime taken is "<<(double)(end - start)/CLOCKS_PER_SEC<<" seconds.\n";
+  cout << "\nTime taken is " << (double)(end - start) / CLOCKS_PER_SEC << " seconds.\n";
 }
 
-int main(){
-  int n,choice;
-  cout<<"Enter the number of elements: ";
-  cin>>n;
+int main()
+{
+  int n, choice;
+  cout << "Enter the number of elements: ";
+  cin >> n;
   vector<int> arr(n);
 
-  cout<<"Using random inputs.\n";
-  for(int i=0;i<n;i++){
-    int a = rand()*(INT_MAX);
+  cout << "Using random inputs.\n";
+  for (int i = 0; i < n; i++)
+  {
+    int a = rand() * (INT_MAX);
     arr[i] = a;
   }
-  while(choice){
-    cout<<"Enter 2 for Bubble Sort, 3 for Insertion Sort or 0 to Exit.\n";
-    cin>>choice;
-    switch(choice){
-      case 0: break;
-      case 2:{
-        BubbleSort(arr);
+  while (choice)
+  {
+    cout << "Enter 2 for Bubble Sort, 3 for Insertion Sort or 0 to Exit.\n";
+    cin >> choice;
+    switch (choice)
+    {
+    case 0:
+      break;
+    case 2:
+    {
+      BubbleSort(arr);
+      break;
+    }
+    case 3:
+    {
+      InsertionSort(arr);
+      break;
+    }
+    default:
+    {
+      cout << "Incorrect option. Enter 1 to Continue or 0 to Exit.\n";
+      cin >> choice;
+      if (!choice)
         break;
-      }
-      case 3:{
-        InsertionSort(arr);
-        break;
-      }
-      default:{
-        cout<<"Incorrect option. Enter 1 to Continue or 0 to Exit.\n";
-        cin>>choice;
-        if(!choice) break;
-        else continue;
-      }
+      else
+        continue;
+    }
     }
   }
   return 0;
